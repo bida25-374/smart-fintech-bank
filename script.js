af6bd79c72084d113bf0fda135b5725ae4c7e774
bida25-374 <bida25-374@thuto.bac.ac.bw>
2026-04-25 18:22:33 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -16,5 +16,32 @@ function validateLogin() {
     } 
 
         alert("Login successful!");
-        return true;
+        return false; // prevent page refresh
+    }
+
+
+    // SAVINGS FORM
+    function submitSavings() {
+        alert("Your savings have been submitted successfully!");
+    }
+
+    // LOAN FORM
+    function submitLoan() {
+        alert("loan application submitted successfully!");
+    }
+
+// CONTACT FORM MESSAGE
+    function submitForm() {
+        const name = document.querySelector('input[name="name"]').value;
+        const email = document.querySelector('input[name="email"]').value;
+        const phone = document.querySelector('input[name="phone"]').value;
+        const message = document.querySelector('input[name="message"]').value;
+
+        if (name === "" || email === "" || phone === "" || message === "") {
+            alert("Please fill in all fields");
+            return false;
+        }
+
+        alert("Your message has been submitted successfully!");
+        return false; // prevent page refresh   
     }
